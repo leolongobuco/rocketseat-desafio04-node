684f16a5639e38c7e3cf383042514d31d82ba0b5
Leonardo Longobuco <longobucoleonardo@gmail.com>
2021-11-12 23:57:59 -0300

    Desafio finalizado

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,14 +2,20 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+  user_id: string | string[];
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(String(user_id));
+
+    if (!user) throw new Error("User doesn't exist");
+
+    if (user.admin === false) throw new Error("This user is not an admin");
+
+    return this.usersRepository.list();
   }
 }
 
